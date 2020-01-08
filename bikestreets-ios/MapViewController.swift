09face9ab2cacf4d1a3e3140ad0bf53d5473e277
Mark Kristensson <mkristensson@mac.com>
2020-01-08 16:17:52 -0700

    Fix issues with map initialization

--- bikestreets-ios/MapViewController.swift
+++ bikestreets-ios/MapViewController.swift
@@ -18,10 +18,15 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        // Street or satellite view?
+        var basemapType: AGSBasemapType = MapViewDefaults.basemapType
+        if UserSettings.mapViewType == MapViewType.satellite.rawValue {
+            basemapType = .imageryWithLabelsVector
+        }
         
         // Display a map using the ArcGIS Online imagery basemap service
-        mapView.map = AGSMap(basemapType: MapViewDefaults.basemapType,
+        mapView.map = AGSMap(basemapType: basemapType,
                              latitude: MapViewDefaults.latitude,
                              longitude: MapViewDefaults.longitude,
                              levelOfDetail: MapViewDefaults.detailLevel)
@@ -32,10 +37,21 @@ class MapViewController: UIViewController {
         // TODO: Versioning scheme for the KML data
         // TODO: Do we have a cached/downloaded version of the KML data?
         // TODO: Is our cached version of KML the latest & greatest?
-        
-        displayCurrentLocation()
-        centerMapOnCurrentLocation()
 
+        // Configure
+        configureMapPerspective()
+        
+        mapView.locationDisplay.useCourseSymbolOnMovement = true
+
+        // Begin the location display (ie show the little dot with the user's location)
+        mapView.locationDisplay.start { [weak self] (error:Error?) -> Void in
+            if let error = error {
+                // TODO: Show an alert through a centralized infrastructure
+//                self?.showAlert(withStatus: error.localizedDescription)
+            }
+        }
+
+        // Style the buttons
         buttonWrapperView.layer.cornerRadius = 5.0
         buttonWrapperView.layer.masksToBounds = true
         
@@ -72,13 +88,10 @@ class MapViewController: UIViewController {
         return kmlLayer
     }
     
+    /**
+     * Change the map's perspective depending upon the user setting
+     */
     func configureMapPerspective() {
-        if UserSettings.mapViewType == MapViewType.satellite.rawValue {
-            mapView.map?.basemap = .imageryWithLabelsVector()
-        } else {
-            mapView.map?.basemap = .lightGrayCanvasVector()
-        }
-        
         // Do we need to prevent the screen from locking?
         UIApplication.shared.isIdleTimerDisabled = UserSettings.preventScreenLockOnMap
 
@@ -87,20 +100,10 @@ class MapViewController: UIViewController {
             mapView.locationDisplay.autoPanMode = AGSLocationDisplayAutoPanMode.navigation
         } else {
             mapView.locationDisplay.autoPanMode = AGSLocationDisplayAutoPanMode.recenter
-            mapView.setViewpointRotation(0.0, completion: nil)
+//            mapView.setViewpointRotation(0.0, completion: nil) // Cannot do this until after location has been determined
         }
     }
     
-    func displayCurrentLocation() {
-        mapView.locationDisplay.useCourseSymbolOnMovement = true
-                
-        mapView.locationDisplay.start { [weak self] (error:Error?) -> Void in
-            if let error = error {
-                // TODO: Show an alert through a centralized infrastructure
-//                self?.showAlert(withStatus: error.localizedDescription)
-            }
-        }
-    }
     /**
      * Recenter the map on the current location, but don't change the zoom level
      */
@@ -111,14 +114,25 @@ class MapViewController: UIViewController {
         }
     }
     
+    /**
+     * Street or Satellite view?
+     */
+    func changeBaseMapType() {
+        if UserSettings.mapViewType == MapViewType.satellite.rawValue {
+            mapView.map?.basemap = .imageryWithLabelsVector()
+        } else {
+            mapView.map?.basemap = .lightGrayCanvasVector()
+        }
+    }
+    
     @objc
     func userDefaultsDidChange(_ notification: Notification) {
         // TODO: Make this more targeted by observing on the values we care about in UserDefaults
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5,
                                       execute: {
+                                        self.changeBaseMapType()
                                         self.configureMapPerspective()
-                                        self.centerMapOnCurrentLocation()
         })
     }
     
