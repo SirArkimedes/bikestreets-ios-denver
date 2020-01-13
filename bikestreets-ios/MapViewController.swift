3cf88a324fd404e5afe9edf11deb28aab03e3a38
Mark Kristensson <mkristensson@mac.com>
2020-01-13 09:46:48 -0700

    Switch to mapbox from ArcGIS, to Carthage from Cocoapods

--- bikestreets-ios/MapViewController.swift
+++ bikestreets-ios/MapViewController.swift
@@ -1,62 +1,49 @@
 
 import UIKit
-import ArcGIS
+import Mapbox
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MGLMapViewDelegate {
 
-    @IBOutlet weak var mapView: AGSMapView!
+    @IBOutlet weak var mapView: MGLMapView!
     @IBOutlet weak var buttonWrapperView: UIView!
     
     // Array to hold on to observer objects for watching changes to UserDefaults
     var userSettingObservers: [NSObject]?
         
+    struct MapStyles {
+        // List of available styles here: https://docs.mapbox.com/api/maps/#styles
+        static let street = URL(string: "mapbox://styles/mapbox/streets-v11")
+        static let satellite = URL(string: "mapbox://styles/mapbox/satellite-v9")
+        static let satelliteWithLabels = URL(string: "mapbox://styles/mapbox/satellite-streets-v9")
+    }
+    
     /**
      * Defaults for the map view
      */
     struct MapViewDefaults {
-        static let basemapType = AGSBasemapType.lightGrayCanvasVector
+        static let mapStyle = MapStyles.street
         static let latitude = 39.7390
         static let longitude = -104.9911
-        static let detailLevel = 15
+        static let detailLevel = 14.0
     }
     
-    // MARK: UIViewController overrides
+    // MARK: - UIViewController overrides
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        mapView.delegate = self
+        
+        // Initlize the map location with our default. We'll center it on the user later (assuming she gives us
+        // permission)
+        mapView.setCenter(CLLocationCoordinate2D(latitude: MapViewDefaults.latitude,
+                                                 longitude: MapViewDefaults.longitude),
+                          zoomLevel: MapViewDefaults.detailLevel,
+                          animated: false)
+
         // Street or satellite view?
-        var basemapType: AGSBasemapType = MapViewDefaults.basemapType
-        if UserSettings.mapViewType == MapViewType.satellite.rawValue {
-            basemapType = .imageryWithLabelsVector
-        }
-        
-        // Display a map using the ArcGIS Online imagery basemap service
-        mapView.map = AGSMap(basemapType: basemapType,
-                             latitude: MapViewDefaults.latitude,
-                             longitude: MapViewDefaults.longitude,
-                             levelOfDetail: MapViewDefaults.detailLevel)
-        
-        
-        loadMapFromShippedResources()
-
-        // TODO: Versioning scheme for the KML data
-        // TODO: Do we have a cached/downloaded version of the KML data?
-        // TODO: Is our cached version of KML the latest & greatest?
-
-        // Configure
+        configureMapStyle()
         configureMapPerspective()
         configureKeepScreenOn()
-        
-        mapView.locationDisplay.useCourseSymbolOnMovement = true
-
-        // Begin the location display (ie show the little dot with the user's location)
-        mapView.locationDisplay.start { [weak self] (error:Error?) -> Void in
-            guard let strongSelf = self else { return }
-            if let error = error {
-                // TODO: Show an alert through a centralized infrastructure
-//                self?.showAlert(withStatus: error.localizedDescription)
-            }
-        }
 
         // Style the buttons
         buttonWrapperView.layer.cornerRadius = 5.0
@@ -69,36 +56,103 @@ class MapViewController: UIViewController {
         return .default
     }
 
-    // MARK: --
+    // MARK: - MGLMapViewDelegate
+    
+    func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
+        // Wait until the map is loaded before adding to the map.
+        loadMapFromShippedResources()
+    }
+    
+    // MARK: - Load Bike Streets Data
     
     /**
      * Load the default Bike Streets map from KML resources files bundled into the app
      */
     func loadMapFromShippedResources() {
-        if let kmlFileUrls = Bundle.main.urls(forResourcesWithExtension: "kml", subdirectory: nil) {
-            for kmlFileUrl in kmlFileUrls {
-                if let layer = loadMapLayerFrom(layerUrl: kmlFileUrl) {
-                    self.mapView.map?.operationalLayers.add(layer)
+        // TODO: Versioning scheme for the geojson data
+        // TODO: Do we have a cached/downloaded version of the geojson data?
+        // TODO: Is our cached version of geojson the latest & greatest?
+
+        DispatchQueue.global().async {
+            if let fileURLs = Bundle.main.urls(forResourcesWithExtension: "geojson", subdirectory: nil) {
+                for fileURL in fileURLs {
+                    guard let jsonData = try? Data(contentsOf: fileURL) else {
+                        preconditionFailure("Failed to parse GeoJSON file")
+                    }
+                    
+                    DispatchQueue.main.async {
+                        self.drawPolyline(geoJson: jsonData)
+                    }
                 }
             }
         }
     }
  
-    func loadMapLayerFrom(layerUrl: URL) -> AGSLayer? {
-        let kmlDataset = AGSKMLDataset(url: layerUrl)
-        let kmlLayer = AGSKMLLayer(kmlDataset: kmlDataset)
-        
-        return kmlLayer
+    func drawPolyline(geoJson: Data) {
+        // Add our GeoJSON data to the map as an MGLGeoJSONSource.
+        // We can then reference this data from an MGLStyleLayer.
+     
+        // MGLMapView.style is optional, so you must guard against it not being set.
+        guard let style = self.mapView.style else { return }
+     
+        guard let shapeFromGeoJSON = try? MGLShape(data: geoJson, encoding: String.Encoding.utf8.rawValue) else {
+            fatalError("Could not generate MGLShape")
+        }
+     
+        let source = MGLShapeSource(identifier: "polyline", shape: shapeFromGeoJSON, options: nil)
+        style.addSource(source)
+     
+        // Create new layer for the line.
+        let layer = MGLLineStyleLayer(identifier: "polyline", source: source)
+     
+        // Set the line join and cap to a rounded end.
+        layer.lineJoin = NSExpression(forConstantValue: "round")
+        layer.lineCap = NSExpression(forConstantValue: "round")
+     
+        // Set the line color to a constant blue color.
+        layer.lineColor = NSExpression(forConstantValue: UIColor(red: 59/255, green: 178/255, blue: 208/255, alpha: 1))
+     
+        // Use `NSExpression` to smoothly adjust the line width from 2pt to 20pt between zoom levels 14 and 18. The `interpolationBase` parameter allows the values to interpolate along an exponential curve.
+        layer.lineWidth = NSExpression(format: "mgl_interpolate:withCurveType:parameters:stops:($zoomLevel, 'linear', nil, %@)",
+        [14: 2, 18: 20])
+         
+        // We can also add a second layer that will draw a stroke around the original line.
+        let casingLayer = MGLLineStyleLayer(identifier: "polyline-case", source: source)
+        // Copy these attributes from the main line layer.
+        casingLayer.lineJoin = layer.lineJoin
+        casingLayer.lineCap = layer.lineCap
+        // Line gap width represents the space before the outline begins, so should match the main line’s line width exactly.
+        casingLayer.lineGapWidth = layer.lineWidth
+        // Stroke color slightly darker than the line color.
+        casingLayer.lineColor = NSExpression(forConstantValue: UIColor(red: 41/255, green: 145/255, blue: 171/255, alpha: 1))
+        // Use `NSExpression` to gradually increase the stroke width between zoom levels 14 and 18.
+        casingLayer.lineWidth = NSExpression(format: "mgl_interpolate:withCurveType:parameters:stops:($zoomLevel, 'linear', nil, %@)", [14: 1, 18: 4])
+         
+        // Just for fun, let’s add another copy of the line with a dash pattern.
+        let dashedLayer = MGLLineStyleLayer(identifier: "polyline-dash", source: source)
+        dashedLayer.lineJoin = layer.lineJoin
+        dashedLayer.lineCap = layer.lineCap
+        dashedLayer.lineColor = NSExpression(forConstantValue: UIColor.white)
+        dashedLayer.lineOpacity = NSExpression(forConstantValue: 0.5)
+        dashedLayer.lineWidth = layer.lineWidth
+        // Dash pattern in the format [dash, gap, dash, gap, ...]. You’ll want to adjust these values based on the line cap style.
+        dashedLayer.lineDashPattern = NSExpression(forConstantValue: [0, 1.5])
+         
+        style.addLayer(layer)
+        style.addLayer(dashedLayer)
+        style.insertLayer(casingLayer, below: layer)
     }
+    
+    // MARK: - Configuration Methods
     
     /**
      * Street or Satellite view?
      */
-    func changeBaseMapType() {
+    func configureMapStyle() {
         if UserSettings.mapViewType == MapViewType.satellite.rawValue {
-            mapView.map?.basemap = .imageryWithLabelsVector()
+            mapView.styleURL = MapStyles.satelliteWithLabels
         } else {
-            mapView.map?.basemap = .lightGrayCanvasVector()
+            mapView.styleURL = MapStyles.street
         }
     }
     
@@ -106,15 +160,19 @@ class MapViewController: UIViewController {
      * Change the map's perspective depending upon the user setting
      */
     func configureMapPerspective(isChange: Bool = false) {
+        mapView.showsUserLocation = true
+        
         // How should we orient the map? North or Direction of Travel?
         if (UserSettings.mapOrientation == MapDirectionOfTravel.directionOfTravel.rawValue) {
-            mapView.locationDisplay.autoPanMode = AGSLocationDisplayAutoPanMode.navigation
+            mapView.userTrackingMode = .followWithHeading
+            mapView.showsUserHeadingIndicator = true
         } else {
-            mapView.locationDisplay.autoPanMode = AGSLocationDisplayAutoPanMode.recenter
+            mapView.userTrackingMode = .follow
+            mapView.showsUserHeadingIndicator = false
+
             if isChange {
                 // We should not change the viewpoint rotation & (re)center the map unless this is a change
                 // in map perspective.
-                mapView.setViewpointRotation(0.0, completion: nil)
                 centerMapOnCurrentLocation()
             }
         }
@@ -131,8 +189,12 @@ class MapViewController: UIViewController {
      * Recenter the map on the current location, but don't change the zoom level
      */
     func centerMapOnCurrentLocation() {
-        if let currentPosition = mapView.locationDisplay.location?.position {
-            mapView.setViewpointCenter(currentPosition)
+        guard let userLocation = mapView.userLocation else {
+            return
+        }
+        
+        if !mapView.isUserLocationVisible {
+            mapView.setCenter(userLocation.coordinate, animated: true)
         }
     }
     
@@ -144,7 +206,7 @@ class MapViewController: UIViewController {
             guard let strongSelf = self else { return }
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5,
                                           execute: {
-                                            strongSelf.changeBaseMapType()
+                                            strongSelf.configureMapStyle()
             })
         }
         userSettingObservers?.append(observer)
@@ -166,7 +228,7 @@ class MapViewController: UIViewController {
         userSettingObservers?.append(observer)
     }
     
-    // MARK: Button Action Methods
+    // MARK: - Button Action Methods
     
     @IBAction func infoButtonTapped(_ sender: Any) {
         let mapSettingsViewController = MapSettingsViewController()
