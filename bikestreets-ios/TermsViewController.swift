331bd6edbc2742ff948958249f7ebf580eaf7f6a
Mark Kristensson <mkristensson@mac.com>
2020-03-07 13:00:15 -0700

    Set Terms text in code

--- bikestreets-ios/TermsViewController.swift
+++ bikestreets-ios/TermsViewController.swift
@@ -5,11 +5,13 @@ import UIKit
 // MARK: -
 class TermsViewController: UIViewController {
     // MARK: - IVARs
+    @IBOutlet weak var termsTextView: UITextView!
     @IBOutlet weak var declineButton: UIButton!
     @IBOutlet weak var acceptButton: UIButton!
     
     // MARK: - View Controller overrides
     override func viewDidLoad() {
+        termsTextView.text = NSLocalizedString("I won't sue Avi!", comment: "This is legal text that must be approved by an attorney.")
         declineButton.titleLabel?.text = NSLocalizedString("Decline", comment: "As in 'Decline' the terms of the app")
         acceptButton.titleLabel?.text = NSLocalizedString("Accept", comment: "As in 'Accept' the terms of the app")
     }
