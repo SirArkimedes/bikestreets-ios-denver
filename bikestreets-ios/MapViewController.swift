8bdbff0ea984fe0203f69380213d65fef846c8fd
Mark Kristensson <mkristensson@mac.com>
2020-04-15 13:03:26 -0600

    Break MapVC methods into extensions

--- bikestreets-ios/MapViewController.swift
+++ bikestreets-ios/MapViewController.swift
@@ -12,7 +12,7 @@ struct MapViewDefaults {
     static let locationArrowOutline = UIImage(named: "location-arrow-outline")
 }
 
-struct MapViewLimits {
+fileprivate struct MapViewLimits {
     static let maxZoomLevel = 19.0
     static let minZoomLevel = 10.0
 }
@@ -27,10 +27,10 @@ class MapViewController: UIViewController {
     @IBOutlet weak var locationButton: UIButton!
     @IBOutlet weak var debugInfoLabel: UILabel!
     
-    let logger = Logger(name: "MapViewController")
+    private let logger = Logger(name: "MapViewController")
     
     // Array to hold on to observer objects for watching changes to UserDefaults
-    var userSettingObservers: [NSObject] = [NSObject]()
+    private var userSettingObservers: [NSObject] = [NSObject]()
             
     // MARK: - UIViewController overrides
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -80,150 +80,6 @@ class MapViewController: UIViewController {
             }
             present(termsViewController, animated: true, completion: nil)
         }
-    }
-    
-    // MARK: - Load Bike Streets Data
-    
-    /**
-     * Load the default Bike Streets map from KML resources files bundled into the app
-     */
-    func loadMapFromShippedResources() {
-        // TODO: Versioning scheme for the geojson data
-        // TODO: Do we have a cached/downloaded version of the geojson data?
-        // TODO: Is our cached version of geojson the latest & greatest?
-
-        if let fileURLs = Bundle.main.urls(forResourcesWithExtension: "geojson", subdirectory: nil) {
-            for fileURL in fileURLs {
-                self.loadMapLayerFrom(fileURL)
-            }
-        }
-    }
- 
-    func loadMapLayerFrom(_ fileURL: URL) {
-        // MGLMapView.style is optional, so you must guard against it not being set.
-        guard let style = self.mapView.style else {
-            return
-        }
-        
-        // Get the layer name from the file name. We'll use it in a couple of places
-        guard let layerName = fileURL.lastPathComponent.layerName() else {
-            fatalError("Unable to locate layer name in file name \(fileURL.lastPathComponent)")
-        }
-
-        // Jump off the main thread to do the heavy lifting of reading the file and parsing the JSON
-        DispatchQueue.global().async {
-            // Get the geoJSON out of the file
-            guard let jsonData = try? Data(contentsOf: fileURL) else {
-                preconditionFailure("Failed to parse GeoJSON file")
-            }
-            
-            // Parse the geoJSON into a Shape object
-            guard let shapeFromGeoJSON = try? MGLShape(data: jsonData, encoding: String.Encoding.utf8.rawValue) else {
-                fatalError("Could not generate MGLShape")
-            }
-            
-            // Create the shape and layer from the JSON
-            let source = MGLShapeSource(identifier: layerName, shape: shapeFromGeoJSON, options: nil)
-            let layer = BikeStreetsStyles.style(forLayer: layerName, source: source)
-            
-            // Jump back to the main thread for the UI work of rendering the shape and layer
-            DispatchQueue.main.async {
-                // Add our GeoJSON data to the map as an MGLGeoJSONSource.
-                // We can then reference this data from an MGLStyleLayer.
-                style.addSource(source)
-                style.addLayer(layer)
-            }
-        }
-    }
-    
-    // MARK: - Map Configuration Methods
-    
-    /**
-     * Street or Satellite view?
-     */
-    func configureMapStyle() {
-        if UserSettings.mapViewType == .satellite {
-            mapView.styleURL = BikeStreetsMapTypes.satelliteWithLabels
-        } else {
-            mapView.styleURL = BikeStreetsMapTypes.bikeStreets
-        }
-    }
-    
-    /**
-     * Change the map's perspective depending upon the user setting
-     */
-    func configureMapPerspective(isChange: Bool = false) {
-        mapView.showsUserLocation = true
-        // How should we orient the map? Fixed or Direction of Travel?
-        if (UserSettings.mapOrientation == .directionOfTravel) {
-            mapView.showsUserHeadingIndicator = true
-        } else {
-            mapView.showsUserHeadingIndicator = false
-
-            if isChange {
-                // We should not change the viewpoint rotation & (re)center the map unless this is a change
-                // in map perspective.
-                centerMapOnCurrentLocation()
-            }
-        }
-    }
-    
-    /**
-     * Do we need to prevent the screen from locking?
-     */
-    func configureKeepScreenOn() {
-        UIApplication.shared.isIdleTimerDisabled = UserSettings.preventScreenLockOnMap
-    }
-    
-    /**
-     * Recenter the map on the current location, but don't change the zoom level
-     */
-    func centerMapOnCurrentLocation() {
-        guard let userLocation = mapView.userLocation else {
-            return
-        }
-        
-        if !mapView.isUserLocationVisible {
-            mapView.setCenter(userLocation.coordinate, animated: true)
-        }
-    }
-    
-    func enableUserTrackingMode() {
-        if (UserSettings.mapOrientation == .directionOfTravel) {
-            mapView.userTrackingMode = .followWithHeading
-        } else {
-            mapView.userTrackingMode = .follow
-        }
-    }
-    
-    /**
-     * Watch for changes to the UserSettings
-     */
-    func configureUserSettingObservers() {
-        var observer = UserSettings.$mapViewTypeRaw.observe { [weak self] old, new in
-            guard let strongSelf = self else { return }
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5,
-                                          execute: {
-                                            strongSelf.configureMapStyle()
-            })
-        }
-        userSettingObservers.append(observer)
-        observer = UserSettings.$mapOrientationRaw.observe { [weak self] old, new in
-            guard let strongSelf = self else { return }
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5,
-                                          execute: {
-                                            strongSelf.configureMapPerspective(isChange: true)
-            })
-        }
-        userSettingObservers.append(observer)
-        observer = UserSettings.$preventScreenLockOnMap.observe { [weak self] old, new in
-            guard let strongSelf = self else { return }
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5,
-                                          execute: {
-                                            strongSelf.configureKeepScreenOn()
-            })
-        }
-        userSettingObservers.append(observer)
     }
     
     // MARK: - Button Action Methods
@@ -291,13 +147,162 @@ extension MapViewController: MGLMapViewDelegate {
     }
 }
 
+// MARK: - Load Bike Streets Data
+extension MapViewController {
+    /**
+     * Load the default Bike Streets map from KML resources files bundled into the app
+     */
+    private func loadMapFromShippedResources() {
+        // TODO: Versioning scheme for the geojson data
+        // TODO: Do we have a cached/downloaded version of the geojson data?
+        // TODO: Is our cached version of geojson the latest & greatest?
+        if let fileURLs = Bundle.main.urls(forResourcesWithExtension: "geojson", subdirectory: nil) {
+            for fileURL in fileURLs {
+               self.loadMapLayerFrom(fileURL)
+            }
+        }
+    }
+    
+    private func loadMapLayerFrom(_ fileURL: URL) {
+        // MGLMapView.style is optional, so you must guard against it not being set.
+        guard let style = self.mapView.style else {
+            return
+        }
+           
+        // Get the layer name from the file name. We'll use it in a couple of places
+        guard let layerName = fileURL.lastPathComponent.layerName() else {
+           fatalError("Unable to locate layer name in file name \(fileURL.lastPathComponent)")
+        }
+
+        // Jump off the main thread to do the heavy lifting of reading the file and parsing the JSON
+        DispatchQueue.global().async {
+        
+            // Get the geoJSON out of the file
+            guard let jsonData = try? Data(contentsOf: fileURL) else {
+                preconditionFailure("Failed to parse GeoJSON file")
+            }
+               
+            // Parse the geoJSON into a Shape object
+            guard let shapeFromGeoJSON = try? MGLShape(data: jsonData, encoding: String.Encoding.utf8.rawValue) else {
+                fatalError("Could not generate MGLShape")
+            }
+               
+            // Create the shape and layer from the JSON
+            let source = MGLShapeSource(identifier: layerName, shape: shapeFromGeoJSON, options: nil)
+            let layer = BikeStreetsStyles.style(forLayer: layerName, source: source)
+               
+            // Jump back to the main thread for the UI work of rendering the shape and layer
+            DispatchQueue.main.async {
+                // Add our GeoJSON data to the map as an MGLGeoJSONSource.
+                // We can then reference this data from an MGLStyleLayer.
+                style.addSource(source)
+                style.addLayer(layer)
+            }
+        }
+    }
+}
+
 // MARK: -
-private extension String {
+fileprivate extension String {
     func layerName() -> String? {
         let fileNameComponents = components(separatedBy: "-")
         if fileNameComponents.count >= 2 {
             return fileNameComponents[1]
         }
         return nil
+    }
+}
+
+// MARK: - Map Configuration Methods
+extension MapViewController {
+    /**
+     * Street or Satellite view?
+     */
+    private func configureMapStyle() {
+        if UserSettings.mapViewType == .satellite {
+            mapView.styleURL = BikeStreetsMapTypes.satelliteWithLabels
+        } else {
+            mapView.styleURL = BikeStreetsMapTypes.bikeStreets
+        }
+    }
+    
+    /**
+     * Change the map's perspective depending upon the user setting
+     */
+    private func configureMapPerspective(isChange: Bool = false) {
+        mapView.showsUserLocation = true
+        // How should we orient the map? Fixed or Direction of Travel?
+        if (UserSettings.mapOrientation == .directionOfTravel) {
+            mapView.showsUserHeadingIndicator = true
+        } else {
+            mapView.showsUserHeadingIndicator = false
+
+            if isChange {
+                // We should not change the viewpoint rotation & (re)center the map unless this is a change
+                // in map perspective.
+                centerMapOnCurrentLocation()
+            }
+        }
+    }
+    
+    /**
+     * Do we need to prevent the screen from locking?
+     */
+    private func configureKeepScreenOn() {
+        UIApplication.shared.isIdleTimerDisabled = UserSettings.preventScreenLockOnMap
+    }
+    
+    /**
+     * Recenter the map on the current location, but don't change the zoom level
+     */
+    private func centerMapOnCurrentLocation() {
+        guard let userLocation = mapView.userLocation else {
+            return
+        }
+        
+        if !mapView.isUserLocationVisible {
+            mapView.setCenter(userLocation.coordinate, animated: true)
+        }
+    }
+    
+    /**
+     * Enable user tracking (i.e. pan/move the map as the user moves)
+     */
+    private func enableUserTrackingMode() {
+        if (UserSettings.mapOrientation == .directionOfTravel) {
+            mapView.userTrackingMode = .followWithHeading
+        } else {
+            mapView.userTrackingMode = .follow
+        }
+    }
+    
+    /**
+     * Watch for changes to the UserSettings
+     */
+    private func configureUserSettingObservers() {
+        var observer = UserSettings.$mapViewTypeRaw.observe { [weak self] old, new in
+            guard let strongSelf = self else { return }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5,
+                                          execute: {
+                                            strongSelf.configureMapStyle()
+            })
+        }
+        userSettingObservers.append(observer)
+        observer = UserSettings.$mapOrientationRaw.observe { [weak self] old, new in
+            guard let strongSelf = self else { return }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5,
+                                          execute: {
+                                            strongSelf.configureMapPerspective(isChange: true)
+            })
+        }
+        userSettingObservers.append(observer)
+        observer = UserSettings.$preventScreenLockOnMap.observe { [weak self] old, new in
+            guard let strongSelf = self else { return }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5,
+                                          execute: {
+                                            strongSelf.configureKeepScreenOn()
+            })
+        }
+        userSettingObservers.append(observer)
     }
 }
