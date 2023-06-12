069420f9d55347a354222b9082bf969a644d6011
Matt Robinson <matt_rob@me.com>
2023-06-12 14:04:59 -0600

    Initial Commit

---
+++ MapboxTester/MapboxTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapboxTesterApp.swift
+//  MapboxTester
+//
+//  Created by Matt Robinson on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MapboxTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
