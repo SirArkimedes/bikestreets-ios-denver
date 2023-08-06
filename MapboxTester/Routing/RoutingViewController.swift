78aa851fe433f8a0978c5ca3c8f79c59a5c5efbf
Matt Robinson <mattrob@hey.com>
2023-08-06 15:28:03 -0600

    Add “End Route” button
    Signed-off-by: Matt Robinson <mattrob@hey.com>

--- MapboxTester/Routing/RoutingViewController.swift
+++ MapboxTester/Routing/RoutingViewController.swift
@@ -11,6 +11,25 @@ import UIKit
 final class RoutingViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .red
+
+    view.backgroundColor = .white
+
+    let stopRoutingButton = UIButton()
+    stopRoutingButton.backgroundColor = .red
+    stopRoutingButton.setTitle("End Route", for: .normal)
+    stopRoutingButton.setTitleColor(.white, for: .normal)
+    stopRoutingButton.titleLabel?.font = .systemFont(ofSize: 24, weight: .bold)
+    stopRoutingButton.layer.cornerRadius = 10
+    stopRoutingButton.clipsToBounds = true
+
+    let stackView = UIStackView(arrangedSubviews: [stopRoutingButton])
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(stackView)
+    NSLayoutConstraint.activate([
+      view.safeAreaLayoutGuide.topAnchor.constraint(equalTo: stackView.topAnchor),
+      view.safeAreaLayoutGuide.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: -16),
+      view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 16),
+      view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: stackView.bottomAnchor),
+    ])
   }
 }
