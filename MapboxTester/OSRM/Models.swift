ac4105189f23da2ce29b3f7b9e2c63881bee58d3
Matt Robinson <mattrob@hey.com>
2023-08-11 15:01:32 -0600

    Add more information to OSRM models
    Signed-off-by: Matt Robinson <mattrob@hey.com>

--- MapboxTester/OSRM/Models.swift
+++ MapboxTester/OSRM/Models.swift
@@ -8,38 +8,105 @@
 import CoreLocation
 import Foundation
 
+struct RouteServiceResponse: Decodable {
+  let code: String
+  let waypoints: [Waypoint]
+  let routes: [Route]
+}
+
+// MARK: - OSRM Models
+
+struct Geometry: Decodable, Equatable {
+  private enum CodingKeys: String, CodingKey {
+    case type
+    case _coordinates = "coordinates"
+  }
+
+  let type: String
+  let _coordinates: [[Double]]
+
+  var coordinates: [CLLocationCoordinate2D] {
+    _coordinates.map { coordinates in
+      CLLocationCoordinate2D(latitude: coordinates[1], longitude: coordinates[0])
+    }
+  }
+}
+
+/// http://project-osrm.org/docs/v5.5.1/api/#route-object
+struct Route: Decodable, Equatable {
+  let distance: Double
+  let duration: Double
+  let geometry: Geometry
+  let legs: [RouteLeg]
+}
+
+/// http://project-osrm.org/docs/v5.5.1/api/#routeleg-object
+struct RouteLeg: Decodable, Equatable {
+  let distance: Double
+  let duration: Double
+  let summary: String
+  let steps: [RouteStep]
+  // let annotation: [Annotation]
+}
+
+/// http://project-osrm.org/docs/v5.5.1/api/#routestep-object
+struct RouteStep: Decodable, Equatable {
+  let distance: Double
+  let duration: Double
+  let geometry: Geometry
+  let name: String
+  // ref
+  // pronunciation
+  // destinations
+
+  // cycling
+  // pushing bike
+  let mode: String
+
+  // let maneuver: StepManeuver
+
+  // let intersections: [Intersection]
+}
+
+/// http://project-osrm.org/docs/v5.5.1/api/#annotation-object
+//struct Annotation: Decodable, Equatable {
+//  let distance: Double
+//  let duration: Double
+//
+//  // datasources
+//  // nodes
+//}
+
+/// http://project-osrm.org/docs/v5.5.1/api/#stepmaneuver-object
+//struct StepManeuver: Decodable, Equatable {
+//  // location
+//  // bearing_before
+//  // bearing_after
+//  // type
+//  // modifier
+//  // exit
+//}
+
+/// http://project-osrm.org/docs/v5.5.1/api/#lane-object
+//struct Lane: Decodable, Equatable {
+//  // indications
+//  // valid
+//}
+
+/// http://project-osrm.org/docs/v5.5.1/api/#intersection-object
+//struct Intersection: Decodable, Equatable {
+//  // location
+//  // bearings
+//  // entry
+//  // in
+//  // out
+//  // lanes
+//}
+
+/// http://project-osrm.org/docs/v5.5.1/api/#waypoint-object
 struct Waypoint: Decodable {
   let name: String
   let location: [Float]
   let distance: Float
   let hint: String
-}
-
-struct Route: Decodable, Equatable {
-  struct Geometry: Decodable, Equatable {
-    private enum CodingKeys: String, CodingKey {
-      case type
-      case _coordinates = "coordinates"
-    }
-
-    let type: String
-    let _coordinates: [[Double]]
-
-    var coordinates: [CLLocationCoordinate2D] {
-      _coordinates.map { coordinates in
-        CLLocationCoordinate2D(latitude: coordinates[1], longitude: coordinates[0])
-      }
-    }
-  }
-
-  let distance: Double
-  let duration: Double
-  let geometry: Geometry
-  //  let legs: [RouteLeg]
-}
-
-struct RouteServiceResponse: Decodable {
-  let code: String
-  let waypoints: [Waypoint]
-  let routes: [Route]
 }
